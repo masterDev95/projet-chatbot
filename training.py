2f819d709a36de6d0b404178ecdca6db8320630f
Alexis Ottina <masdevelopper@gmail.com>
2022-07-02 00:26:45 +0200

    Training du jeu de données

--- training.py
+++ training.py
@@ -22,8 +22,8 @@ lemmatizer = WordNetLemmatizer()
 # listes
 words = [] # pour chaque mots
 classes = [] # pour chaque classe de mots
-documents = []
-ignore_letters = ['?', '!', '.', ',']
+documents = [] # documents associants classes et mots
+ignore_letters = ['?', '!', '.', ','] # charactères à ignorer
 
 # parcourir avec une boucle For toutes les intentions
 # tokéniser chaque pattern et ajouter les tokens à la liste words, les patterns et
@@ -43,5 +43,33 @@ for intent in intents['intents']:
 words = [lemmatizer.lemmatize(word)
          for word in words if word not in ignore_letters]
 words = sorted(set(words))
+classes = sorted(set(classes))
 
-print(words)
+# on garde les mots et classes dans un fichier
+pickle.dump(words, open('words.pk1', 'wb'))
+pickle.dump(classes, open('classes.pk1', 'wb'))
+
+# liste pour le jeu de données
+training = []
+output_empty = [0] * len(classes)
+
+# création du modèle du jeu de données
+for document in documents:
+    bow = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(
+        word.lower()) for word in word_patterns]
+    for word in words:
+        bow.append(1) if word in word_patterns else bow.append(0)
+        
+    # association du pattern à la classe correspondante
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bow, output_row])
+    
+# mélange du jeu de données et convertion en array
+random.shuffle(training)
+training = numpy.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
