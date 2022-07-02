0c9d46230c4fc15c03386782e7b2bbea6bc9ba5f
Alexis Ottina <masdevelopper@gmail.com>
2022-07-02 13:32:52 +0200

    Modélistaion du DLearning

--- training.py
+++ training.py
@@ -73,3 +73,17 @@ training = numpy.array(training)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
+
+# définition du modèle du Deep Learning
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# compilation du modèle en SGD
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+print(model.summary())
